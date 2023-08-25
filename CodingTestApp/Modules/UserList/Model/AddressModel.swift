f13377b75117ee9be795ba121a790fa82135709d
deq <deq@deqs-MacBook-Pro.local>
2023-08-25 23:32:02 +0530

    Response model updated.

--- CodingTestApp/Modules/UserList/Model/AddressModel.swift
+++ CodingTestApp/Modules/UserList/Model/AddressModel.swift
@@ -12,22 +12,4 @@ struct Address : Codable {
     let city : String?
     let zipcode : String?
     let geo : Geo?
-
-    enum CodingKeys: String, CodingKey {
-        case street = "street"
-        case suite = "suite"
-        case city = "city"
-        case zipcode = "zipcode"
-        case geo = "geo"
-    }
-
-    init(from decoder: Decoder) throws {
-        let values = try decoder.container(keyedBy: CodingKeys.self)
-        street = try values.decodeIfPresent(String.self, forKey: .street)
-        suite = try values.decodeIfPresent(String.self, forKey: .suite)
-        city = try values.decodeIfPresent(String.self, forKey: .city)
-        zipcode = try values.decodeIfPresent(String.self, forKey: .zipcode)
-        geo = try values.decodeIfPresent(Geo.self, forKey: .geo)
-    }
-
 }
